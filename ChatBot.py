42bfc7c4063ed6f5099f8056d7f971508835bd4d
fatih <mrfatiharslann@gmail.com>
2025-07-17 03:04:05 +0300

    main ifadesi düzeltildi

--- ChatBot.py
+++ ChatBot.py
@@ -1,60 +1,209 @@
-import os
 import telebot
+from telebot import types
 
-like_str = "1"
-dislike_str = "2"
+from config import CHANNEL, GROUP, OWNER, TOKEN
+from dataEgine import *
+from Messages import *
 
-# Bot tokenını ortam değişkeninden alıyoruz
-TOKEN = os.getenv("BOT_TOKEN")
-bot = telebot.TeleBot(TOKEN)
+access_token = "8028820851:AAFKml701OfM4Adao5oN_Jqkn5gKqOL9ikc"
+bot = telebot.TeleBot(access_token)
 
-# Kullanıcılar arası iletişim eşleşmesini tutan global sözlük
-communications = {}
 
-# Kullanıcı eşleştirme fonksiyonu
-def add_communications(user1, user2):
-    communications[user1] = {"UserTo": user2}
-    communications[user2] = {"UserTo": user1}
+def inline_menu():
+    """
+    Create inline menu for new chat
+    :return: InlineKeyboardMarkup
+    """
+    callback = types.InlineKeyboardButton(
+        text="\U00002709 New chat", callback_data="NewChat"
+    )
+    kenkan = types.InlineKeyboardButton(text="🔵 ᴏᴡɴᴇʀ", url=f"t.me/{OWNER}")
+    group = types.InlineKeyboardButton(text="👥 ɢʀᴏᴜᴘ", url=f"https://t.me/{GROUP}")
+    channel = types.InlineKeyboardButton(
+        text="ᴄʜᴀɴɴᴇʟ 📣", url=f"https://t.me/{CHANNEL}"
+    )
+    menu = types.InlineKeyboardMarkup()
+    menu.add(kenkan, channel, group, callback)
 
-# Kullanıcının eşleşip eşleşmediğini kontrol eder
+    return menu
+
+
+def generate_markup():
+    """
+    Create menu with two buttons: 'Like' and 'Dislike'
+    :return: ReplyKeyboardMarkup
+    """
+    markup = types.ReplyKeyboardMarkup(one_time_keyboard=False, resize_keyboard=True)
+    markup.add(like_str)
+    markup.add(dislike_str)
+    return markup
+
+
 def connect_user(user_id):
-    return user_id in communications and "UserTo" in communications[user_id]
+    """
+    :param user_id: Chat id with user
+    :return: boolean
+    """
+    if user_id in communications:
+        return True
+    else:
+        bot.send_message(user_id, m_has_not_dialog)
+        return False
 
-# Mesaj işleme fonksiyonu
-@bot.message_handler(content_types=["text", "sticker", "photo", "audio", "video", "voice"])
-def handle_messages(message):
-    user_id = message.from_user.id
 
+@bot.message_handler(commands=["start"])
+def echo(message):
+    """
+    Make the user in Data Base.
+    :param message:
+    :return:
+    """
+    message.chat.type = "private"
+    user_id = message.chat.id
+
+    if message.chat.username is None:
+        bot.send_message(user_id, m_is_not_user_name)
+        return
+
+    menu = inline_menu()
+
+    bot.send_message(user_id, m_start, reply_markup=menu)
+
+
+@bot.message_handler(commands=["stop"])
+def echo(message):
+    """
+    This function remove user from Data Base and sends a farewell message.
+    :param message:
+    :return:
+    """
+    menu = types.ReplyKeyboardRemove()
+    user_id = message.chat.id
+
+    if message.chat.id in communications:
+
+        bot.send_message(
+            communications[user_id]["UserTo"], m_disconnect_user, reply_markup=menu
+        )
+
+        tmp_id = communications[user_id]["UserTo"]
+        delete_info(tmp_id)
+
+    delete_user_from_db(user_id)
+
+    bot.send_message(user_id, m_good_bye)
+
+
+@bot.message_handler(
+    func=lambda call: call.text == like_str or call.text == dislike_str
+)
+def echo(message):
+    """
+    This function reacts to pressing buttons: 'Like' and 'Dislike'
+    If both users press 'Like', then bot sends them username from telegram.
+    If somebody press 'Dislike', then chat finish.
+    :param message:
+    :return:
+    """
+    user_id = message.chat.id
+
+    if user_id not in communications:
+        bot.send_message(user_id, m_failed, reply_markup=types.ReplyKeyboardRemove())
+        return
+
+    user_to_id = communications[user_id]["UserTo"]
+
+    flag = False
+
+    if message.text == dislike_str:
+        bot.send_message(
+            user_id, m_dislike_user, reply_markup=types.ReplyKeyboardRemove()
+        )
+        bot.send_message(
+            user_to_id, m_dislike_user_to, reply_markup=types.ReplyKeyboardRemove()
+        )
+        flag = True
+    else:
+        bot.send_message(user_id, m_like, reply_markup=types.ReplyKeyboardRemove())
+
+        update_user_like(user_to_id)
+
+        if communications[user_id]["like"]:
+            bot.send_message(user_id, m_all_like(communications[user_id]["UserName"]))
+            bot.send_message(
+                user_to_id, m_all_like(communications[user_to_id]["UserName"])
+            )
+            flag = True
+
+    if flag:
+        delete_info(user_to_id)
+        menu = inline_menu()
+        bot.send_message(user_id, m_play_again, reply_markup=menu)
+        bot.send_message(user_to_id, m_play_again, reply_markup=menu)
+
+
+@bot.message_handler(
+    content_types=["text", "sticker", "video", "photo", "audio", "voice"]
+)
+def echo(message):
+    """
+    Resend message to anonymous friend.
+    :param message:
+    :return:
+    """
+    user_id = message.chat.id
     if message.content_type == "sticker":
         if not connect_user(user_id):
             return
-        bot.send_sticker(communications[user_id]["UserTo"], message.sticker.file_id)
 
+        bot.send_sticker(communications[user_id]["UserTo"], message.sticker.file_id)
     elif message.content_type == "photo":
         if not connect_user(user_id):
             return
-        file_id = message.photo[-1].file_id
-        bot.send_photo(communications[user_id]["UserTo"], file_id, caption=message.caption)
 
+        file_id = None
+
+        for item in message.photo:
+            file_id = item.file_id
+
+        bot.send_photo(
+            communications[user_id]["UserTo"], file_id, caption=message.caption
+        )
     elif message.content_type == "audio":
         if not connect_user(user_id):
             return
-        bot.send_audio(communications[user_id]["UserTo"], message.audio.file_id, caption=message.caption)
 
+        bot.send_audio(
+            communications[user_id]["UserTo"],
+            message.audio.file_id,
+            caption=message.caption,
+        )
     elif message.content_type == "video":
         if not connect_user(user_id):
             return
-        bot.send_video(communications[user_id]["UserTo"], message.video.file_id, caption=message.caption)
 
+        bot.send_video(
+            communications[user_id]["UserTo"],
+            message.video.file_id,
+            caption=message.caption,
+        )
     elif message.content_type == "voice":
         if not connect_user(user_id):
             return
-        bot.send_voice(communications[user_id]["UserTo"], message.voice.file_id)
 
+        bot.send_voice(communications[user_id]["UserTo"], message.voice.file_id)
     elif message.content_type == "text":
-        if message.text not in ["/start", "/stop", dislike_str, like_str, "NewChat"]:
+        if (
+            message.text != "/start"
+            and message.text != "/stop"
+            and message.text != dislike_str
+            and message.text != like_str
+            and message.text != "NewChat"
+        ):
+
             if not connect_user(user_id):
                 return
+
             if message.reply_to_message is None:
                 bot.send_message(communications[user_id]["UserTo"], message.text)
             elif message.from_user.id != message.reply_to_message.from_user.id:
@@ -66,9 +215,16 @@ def handle_messages(message):
             else:
                 bot.send_message(user_id, m_send_some_messages)
 
-# Callback sorgu işleyici
+
 @bot.callback_query_handler(func=lambda call: True)
 def echo(call):
+    """
+    Create new chat.
+    All users are divided into two categories: receivers and emitters.
+    If bot finds pair, then it creates new chat.
+    :param call:
+    :return:
+    """
     if call.data == "NewChat":
         user_id = call.message.chat.id
         user_to_id = None
@@ -98,9 +254,8 @@ def echo(call):
         bot.send_message(user_id, m_is_connect, reply_markup=keyboard)
         bot.send_message(user_to_id, m_is_connect, reply_markup=keyboard)
 
-def recovery_data():
-    print("🔄 Recovery başlatıldı... (şimdilik sadece test için çalışıyor)")
 
 if __name__ == "__main__":
     recovery_data()
+    bot.stop_polling()
     bot.polling(none_stop=True)
